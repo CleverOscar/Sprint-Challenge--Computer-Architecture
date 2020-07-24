0af9e9d105642cf03cd4e430cd558aefc47ae14d
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-24 10:34:04 -0500

    Basic Logic for CPU

---
+++ cpu.py
@@ -0,0 +1,23 @@
+import sys
+
+class CPU:
+
+    def __init__(self):
+        self.ram = [None] * 256
+        self.pc = 0
+        self.commands = {}
+
+    def load():
+        pass
+
+    def alu():
+        pass
+
+    def ram_read():
+        pass
+
+    def ram_write():
+        pass
+
+    def run():
+        pass
